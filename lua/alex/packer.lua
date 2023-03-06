038f5761444f541ae02ce492d6039ac4362663ed
Lachance, Alexandre <alexandrelachance@me.com>
2023-03-06 15:18:57 -0500

    Add autopairs.

--- lua/alex/packer.lua
+++ lua/alex/packer.lua
@@ -12,6 +12,7 @@ return require('packer').startup(function(use)
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use 'nvim-treesitter/playground'
+    use 'windwp/nvim-autopairs'
 
     use {
         'nvim-tree/nvim-tree.lua',
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -144,6 +144,11 @@ _G.packer_plugins = {
     path = "/home/alex/.local/share/nvim/site/pack/packer/start/material.nvim",
     url = "https://github.com/marko-cerovac/material.nvim"
   },
+  ["nvim-autopairs"] = {
+    loaded = true,
+    path = "/home/alex/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    url = "https://github.com/windwp/nvim-autopairs"
+  },
   ["nvim-cmp"] = {
     loaded = true,
     path = "/home/alex/.local/share/nvim/site/pack/packer/start/nvim-cmp",
