03b1436c0c0771ae64269ffb69a7e89c753b5d65
Lachance, Alexandre <alexandrelachance@me.com>
2023-08-01 16:42:43 -0400

    Small fixes.

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,36 +1,57 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.set_sign_icons({
-  error = '✘',
-  warn = '▲',
-  hint = '⚑',
-  info = ''
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = ''
 })
 
 vim.diagnostic.config({
-  virtual_text = true,
-  severity_sort = true,
-  float = {
-    style = 'minimal',
-    border = 'rounded',
-    source = 'always',
-    header = '',
-    prefix = '',
-  },
+    virtual_text = true,
+    severity_sort = true,
+    float = {
+        style = 'minimal',
+        border = 'rounded',
+        source = 'always',
+        header = '',
+        prefix = '',
+    },
 })
 
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero.cmp').action()
+
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'buffer', keyword_length = 3},
-    {name = 'luasnip', keyword_length = 2},
-  },
-  window = {
-    documention = cmp.config.window.bordered(),
-  },
+    sources = {
+        { name = 'path' },
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lua' },
+        { name = 'buffer',  keyword_length = 3 },
+        { name = 'luasnip', keyword_length = 2 },
+    },
+    window = {
+        documention = cmp.config.window.bordered(),
+    },
+    mapping = {
+        -- confirm completion item
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+        -- toggle completion menu
+        ['<C-e>'] = cmp_action.toggle_completion(),
+
+        -- tab complete
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
+        -- navigate between snippet placeholder
+        ['<C-d>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+
+        -- scroll documention window
+        ['<C-f>'] = cmp.mapping.scroll_docs(5),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-5),
+    },
 })
