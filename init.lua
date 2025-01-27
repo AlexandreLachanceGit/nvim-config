81c41bff354f79dc444351cf8dd7f66b6fb5193c
Lachance, Alexandre <alexandrelachance@me.com>
2025-01-26 23:27:09 -0500

    Update Telescope version

--- init.lua
+++ init.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
 
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.1",
+        tag = "0.1.8",
         dependencies = { "nvim-lua/plenary.nvim" }
     },
 
